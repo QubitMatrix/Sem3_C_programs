19a7cb055d8fd378c17b74ac7968b0337bf0e76b
Preethi <preethi13m@gmail.com>
2022-10-15 12:58:38 +0530

    Matrix multiplication using dma(single pointer)

--- mul.c
+++ mul.c
@@ -1,12 +1,13 @@
-//SMA:
-#include<stdio.h>
-void mul(int c1,int c2;int a[][c1],int b[][c2],int c[][c2],int r1,int c1,int r2,int c2)
+//DMA:using double pointer
+ #include<stdio.h>
+ #include<stdlib.h>
+void mul(int **a,int **b,int **c,int r1,int c1,int c2)
 {
 for(int i=0;i<r1;i++)
 {
 for(int j=0;j<c2;j++)
 {
-*(*(c+i)+j)=0;
+c[i][j]=0;
 for(int k=0;k<c1;k++)
 {
 *(*(c+i)+j)+=*(*(a+i)+k)*(*(*(b+k)+j));
@@ -14,24 +15,33 @@ for(int k=0;k<c1;k++)
 }
 }
 }
-
-void inp(int c;int (*a)[c],int r,int c)
+int** allocat(int r,int c)
+{
+int **ptr;
+ptr=malloc(r*sizeof(int*));
+for(int i=0;i<c;i++)
+{
+ptr[i]=malloc(c*sizeof(int));
+}
+return ptr;
+}
+void inp(int **ptr,int r,int c)
 {
 for(int i=0;i<r;i++)
 {
 for(int j=0;j<c;j++)
 {
-scanf("%d",(*(a+i)+j));
+scanf("%d",(*(ptr+i)+j));
 }
 }
 }
-void out(int c;int a[][c],int r,int c)
+void out(int **ptr,int r,int c)
 {
 for(int i=0;i<r;i++)
 {
 for(int j=0;j<c;j++)
 {
-printf("%d ",*(*(a+i)+j));
+printf("%d ",*(*(ptr+i)+j));
 }
 printf("\n");
 }
@@ -40,19 +50,18 @@ int main()
 {
 
 int r1,c1,r2,c2;
-int x[]={1,2,3};
-int *p=x;
+int **ma,**mb,**mc;
 printf("Enter the row1 col1 ro2 col2\n");
 scanf("%d %d %d %d",&r1,&c1,&r2,&c2);
-int a[r1][c1];int b[r2][c2];int c[r1][c2];
+ma=allocat(r1,c1);
+mb=allocat(r2,c2);
+mc=allocat(r1,c2);
 printf("Enter elements of first\n");
-inp(a,r1,c1);
-//printf("%lu %lu %lu %lu",x[0],*p,x[1],*(p+1));
+inp(ma,r1,c1);
 printf("Enter elements of second\n");
-inp(b,r2,c2);
-mul(a,b,c,r1,c1,r2,c2);
+inp(mb,r2,c2);
+mul(ma,mb,mc,r1,c1,c2);
 printf("Multiplied matrix:\n");
-out(c,r1,c2);
+out(mc,r1,c2);
 return 0;
 }
-

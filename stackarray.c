8ba3ba10462aa36f01bfccdae3c3ae5f9420fee0
Preethi <preethi13m@gmail.com>
2022-10-15 16:49:30 +0530

    implementation of stack using array

---
+++ stackarray.c
@@ -0,0 +1,93 @@
+#include<stdio_ext.h>
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+	char  *ptr_s;
+	int size;
+	int top;
+};
+typedef struct stack stack;
+void createstack(stack* ptr)
+{
+	ptr->ptr_s=malloc(sizeof(char)*ptr->size);
+	ptr->top=-1;
+	//printf("%p",ptr->ptr_s);
+} 
+void push(stack* ptr,char ele)
+{
+	if(ptr->top==(ptr->size)-1)
+	printf("Stack is full(overflow)");
+	else
+	{
+		ptr->ptr_s[++(ptr->top)]=ele;
+	}
+}	
+char pop(stack* ptr)
+{
+	char ele;
+	if(ptr->top==-1)
+	{
+		return -1;
+	}
+	else
+	{
+		ele=ptr->ptr_s[ptr->top--];
+		return ele;
+	}
+}
+void display(stack st)
+{
+	printf("Stack display:\n");
+	if(st.top==-1)
+	printf("Empty stack\n");
+	else
+	{
+	while(st.top!=-1)
+	{
+		printf("%c\n",st.ptr_s[st.top--]);
+	}
+	}
+}
+int main()
+{
+	stack* ptr_st;
+	ptr_st=malloc(sizeof(stack));
+	printf("Enter the size of the stack");
+	__fpurge(stdin);
+	scanf("%d",&ptr_st->size);
+	createstack(ptr_st);
+	int choice;
+	char ele,res;
+	printf("Enter:\n1. for push\n2. for pop\n3. for display\n4. to exit\n");
+	while(1)
+	{
+	printf("\nEnter your choice\n");
+	__fpurge(stdin);
+	scanf("%d",&choice);
+	printf("Choice:%d\n",choice);
+	switch(choice)
+	{
+		case 1:printf("Enter the element\n");
+		__fpurge(stdin);
+		scanf(" %c",&ele);
+		push(ptr_st,ele);
+		break;
+		case 2:res=pop(ptr_st);
+		if(res==-1)
+		printf("Empty stack(underflow)\n");
+		else
+		printf("Element popped=%c\n",res);
+		break;
+		case 3:display(*ptr_st);
+		break;
+		case 4:exit(0);
+		break;
+		default:printf("Wrong choice\n");
+	}
+	}
+	return 0;
+}
+	
+	
+	

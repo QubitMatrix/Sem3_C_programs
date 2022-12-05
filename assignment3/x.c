292c0a86ef128460e64f79dc807e714971f5a088
Preethi <preethi13m@gmail.com>
2022-12-05 08:13:59 +0530

    added questions

---
+++ assignment3/x.c
@@ -0,0 +1,15 @@
+// write a function to print all the words of trie tree(hint use dfs)
+// write a function to merge two singly linked list 
+// write a recursive function to reverse a singly linked list
+// write a c function to find union of two singly linked list
+// write a c function to check if given tree is a bst or not 
+// write a c function to find predessecor in a threaded bst
+// using circular linked list write a c function to solve josephus problem
+// write a c function to generate a mirror copy of binary tree(shift left child to right and vice versa) this is stored in new tree i.e different address
+// mirrorify a tree(flip the same tree i.e it should be in same address)
+// a single listis used to represent a long integer, ur given two long integer in linked lists(front insertion), write a c function to return the product of these two as a linked list(sum=sum*10+digit)
+// write a c function to find the node at which two sorted singly linked list intersect also find count of nodes in both lists until point of intersection//
+//write a c function to find the intersection set of two lists
+//consider the given figure which consists of two sorted linked list pointed byy start1 and start2. The nde wuth address 3k represents the intersection point.(check which point both point to same address)
+//consider a sorted doubly linked list with two pointers head and tail pointer, write a c function to find all the pairs form this list which add up to the given value x
+
